f3712019e91dd48ddd623de6c46604ba6db61560
faliks <243650719@qq.com>
2021-10-01 12:37:00 +0800

    从Beta版升级为正式版

--- manage.py
+++ manage.py
@@ -16,21 +16,22 @@ def job():
         email_configure = configure["mail"]
         need_send = email_configure["need_send"]
         mail = None
+        message = ""
         if need_send == "True":
             mail = Mail(email_configure["token"], email_configure["sender"], email_configure["receivers"])
         for student in students:
             s = get_login_session(student["account"], student["password"])
             if 'iPlanetDirectoryPro' not in s.cookies:
-                message = student["name"] + " fail"
+                message += student["name"] + " fail\n"
             else:
                 if sign(s):
-                    message = student["name"] + " success"
+                    message += student["name"] + " success\n"
                 else:
-                    message = student["name"] + " fail"
+                    message += student["name"] + " fail\n"
             print(message)
-            if mail is not None:
-                subject = time.strftime("%Y-%m-%d %H:%M", time.localtime()) + "打卡结果"
-                mail.send(subject, message)
+        if mail is not None:
+            subject = time.strftime("%Y-%m-%d %H:%M", time.localtime()) + "打卡结果"
+            mail.send(subject, message)
 
 
 def sign_thread(threadName):
